92dd041e52cbeedd9d495cf9350615340bb83c3f
James McCray <chrisjamesjackmccray@gmail.com>
2016-06-14 22:24:29 -0500

    Commit

--- app/app.js
+++ app/app.js
@@ -9,17 +9,6 @@ import events from './modules/events';
 
 let App = angular.module('app', ['ui.router', 'tiy.characters', 'tiy.events']);
 
-/*
-  App Configuration Function
-  --------------------------
-
-  This is where you pass in config time options to Providers.
-  For example, this is where you can configure $stateProvider
-  to set up your routing.
-
-  STEP 1: Use $urlRouterProvider.otherwise() to default to
-          /characters/Captain America
-*/
 
 function config($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/characters/Captain America')
